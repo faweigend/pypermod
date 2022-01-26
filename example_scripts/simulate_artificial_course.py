69302d6cb331c935373d0d5f45f45095358a8b61
faweigend <fabian.weigend@westernsydney.edu.au>
2022-01-26 16:13:48 +0100

    add one-second offset between power data and predicted energy in simulate artificial course

--- example_scripts/simulate_artificial_course.py
+++ example_scripts/simulate_artificial_course.py
@@ -30,17 +30,19 @@ if __name__ == "__main__":
 
     inter = 180
     # the power demands over the artificial course
-    course = [100] * inter + \
-             [250] * inter + \
-             [100] * inter + \
-             [250] * inter + \
-             [50] * inter + \
-             [50] * inter + \
-             [50] * inter + \
-             [300] * inter + \
-             [200] * inter
-    # we start with second 1 because simulations omit time step 0
-    times = np.arange(1, len(course) + 1)
+    pow_course = [100] * inter + \
+                 [250] * inter + \
+                 [100] * inter + \
+                 [250] * inter + \
+                 [50] * inter + \
+                 [50] * inter + \
+                 [50] * inter + \
+                 [300] * inter + \
+                 [200] * inter
+    pow_times = np.arange(len(pow_course))
+
+    # we start predicted W'bal with second 1 because simulations omit the initial time step 0
+    sim_times = pow_times + 1
 
     agents = [agent_ode, agent_wei, agent_bar]
 
@@ -51,40 +53,33 @@ if __name__ == "__main__":
 
     # use simulator for every agent and plot result
     for agent in agents:
-        balances = SimulatorBasis.simulate_course(agent, course_data=course)
-        ax2.plot(times,
+        balances = SimulatorBasis.simulate_course(agent, course_data=pow_course)
+        ax2.plot(sim_times,
                  balances,
                  color=PlotLayout.get_plot_color(agent.get_name()),
                  label=PlotLayout.get_plot_label(agent.get_name()))
 
     # plot power demands
-    ax.plot(times, course, color='black', label="intensity")
+    ax.plot(pow_times, pow_course, color='black', label="intensity")
 
     # format axis
     ax.set_ylim([0, cp * 2.5])
-
-    # label plot
     ax.set_ylabel("power output (watts)")
-    ax2.set_ylabel(r'$W^\prime$' + " balance (joules)")
     ax.set_xlabel("time (min)")
-
     ax.set_yticks([agent_ode.cp])
     ax.set_yticklabels(["CP"])
     ax.tick_params(axis='y', colors='red')
     ax.axhline(y=cp, linestyle="--", color='red', label="critical power (CP)")
+    # format x ticks to minutes
+    ax.set_xticks(range(0, len(pow_course) + 5, 180))
+    ax.set_xticklabels(int(x / 60) for x in ax.get_xticks())
 
+    ax2.set_ylabel(r'$W^\prime$' + " balance (joules)")
     ax2.set_yticks([0, agent_ode.w_p])
     ax2.set_yticklabels([0, r'$W^\prime$'])
-
-    # format x ticks to minutes
-    # ax.set_xticks(range(0, len(course) + 5, 180))
-    # ax.set_xticklabels(int(x / 60) for x in ax.get_xticks())
-
-    # legends
     ax2.legend()
 
     # formant plot
     locs, labels = plt.xticks()
-    # plt.setp(labels, rotation=-45)
     plt.tight_layout()
     plt.show()
