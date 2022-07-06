6efdd82067cb3e11a82ec99fed7e005d562b8477
faweigend <fabian.weigend@westernsydney.edu.au>
2022-07-06 20:11:32 +1000

    delete leftover code in comments

--- example_scripts/vo2_study/compare_cp_fittings.py
+++ example_scripts/vo2_study/compare_cp_fittings.py
@@ -4,7 +4,6 @@ import os
 import pandas as pd
 
 from pypermod.data_structure.athlete import Athlete
-from pypermod.fitter.cp_to_tte_fitter import CPMTypes
 from pypermod.data_structure.activities.activity_types import ActivityTypes
 from pypermod import config
 
@@ -24,17 +23,6 @@ def cp_fitting_table():
         cp_fitting = athlete.get_cp_fitting_of_type(a_type=ActivityTypes.SRM_BBB_TEST)
 
         vals = []
-
-        # check the 2 parameter models to compare
-        # for p2_model in [CPMTypes.P2_LINEAR, CPMTypes.P2_HYP, CPMTypes.P2_1dTIME]:
-        #     params = cp_fitting.get_params(p2_model)
-        #
-        #     # SEE% value were precomputed by fitting object
-        #     cp_see_p = params["cp_see%"]
-        #     w_p_see_p = params["wp_see%"]
-        #
-        #     # append to values object
-        #     vals += [w_p_see_p, cp_see_p]
 
         # get best fit
         b_fit = cp_fitting.get_best_2p_fit()
